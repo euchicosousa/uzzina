d3ba3b9e3ef8df37a70de72d8f64c4c8cf819bc1
Chico Sousa <ochicosousa@gmail.com>
2026-05-02 11:05:37 -0300

    style: adjust horizontal margins of HomeComponentWrapper for mobile responsiveness

--- app/components/features/home/HomeComponentWrapper.tsx
+++ app/components/features/home/HomeComponentWrapper.tsx
@@ -10,7 +10,7 @@ export function HomeComponentWrapper({
   title: string | ReactNode;
 }) {
   return (
-    <div className="border_after relative mx-8 border-x pb-12">
+    <div className="border_after relative mx-4 border-x pb-12 lg:mx-8">
       <div className="flex flex-col justify-between gap-8 px-8 lg:flex-row lg:items-center xl:px-16">
         <h1 className="py-12 text-5xl font-semibold">{title}</h1>
         {OptionsComponent && (
