a0caf794281e46af7d66b1dcfbbf6cf09ba15fbb
Chico Sousa <ochicosousa@gmail.com>
2026-07-13 13:11:31 -0300

    chore: remove unused scratch file for testing tiptap extensions

--- scratch.js
+++
@@ -1,4 +0,0 @@
-const { Extension } = require('@tiptap/core');
-const StarterKit = require('@tiptap/starter-kit').default;
-const extensions = StarterKit.configure({}).config.addExtensions();
-console.log("StarterKit extensions:", extensions.map(e => e.name));
