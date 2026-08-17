9bfa2c4db3a27c497256e51c53e2a86e6c3b33f1
Chico Sousa <ochicosousa@gmail.com>
2026-08-17 10:46:18 -0300

    style: remove border from footer and reorder anchor tag attributes

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,14 +1,14 @@
 export const Footer = () => (
-  <div className="flex h-24 shrink-0 items-center justify-between border-t px-4 py-4 text-xs text-gray-500 lg:px-8">
+  <div className="flex h-24 shrink-0 items-center justify-between px-4 py-4 text-xs text-gray-500 lg:px-8">
     <p>
       © 2026. Todos os direitos reservados. Feito com carinho pela Agência
       CNVT®.
     </p>
     <a
-      href="https://cnvt.com.br"
-      target="_blank"
       className="font-bold"
+      href="https://cnvt.com.br"
       rel="noopener noreferrer"
+      target="_blank"
     >
       CNVT
     </a>
